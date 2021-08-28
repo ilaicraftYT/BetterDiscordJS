0ba23e08d34a03373afb7aece7ce83ce04704a20
ilaicraftYT <70978042+ilaicraftYT@users.noreply.github.com>
2021-08-27 20:58:15 -0500

    hi

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BetterDiscordJS
 BetterDiscordJS is a NPM package who will help you to make your bots better.
+
+# Usage
+In this examples we are assuming that you added BetterDiscordJS as `util`. 
+
+```js
+const util = require("betterdiscordjs")
+```
+## Ship Image
+```js
+await util.shipImage("avatar 1", "avatar 2")
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const Canvas = require("canvas");
 const Discord = require("discord.js")
+const emojiRegex = require('emoji-regex/RGI_Emoji.js');
+
+/**
+* @date 2021-08-27
+* @param {String} user1 | User 1 Avatar
+* @param {String} user2 | User 2 Avatar
+* @param {Number} porcentage | Porcentage of love
+* @returns {Buffer}
+*/
 
 async function shipImage(user1, user2, porcentage){
     
+    if(!porcentage) throw new TypeError("Porcentage was not set.")
+    if(!user1) throw new TypeError("User 1 Avatar was not set")
+    if(!user2) throw new TypeError("User 2 Avatar was not set")
+    if(typeof user1 !== "string") throw new TypeError("User 1 Avatar isn't a string")
+    if(typeof user2 !== "string") throw new TypeError("User 2 Avatar isn't a string")
+    if(typeof porcentage !== "number") throw new TypeError("Porcentage isn't a string")
+
     let emoji;
 
     if(porcentage <= 23){
@@ -37,6 +53,49 @@ let emoji2 = await Canvas.loadImage(emoji)
 return lienzo.toBuffer();
 }
 
+
+/**
+* @date 2021-08-27
+* @param {String} text | Text to convert
+* @returns {String}
+*/
+function emojify(text) {
+  if(!text) throw new TypeError("Text isn't set.")
+  if(typeof text == "string") throw new TypeError("Text isn't a string.")
+  let customEmoteRegex = /^(:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>)+$/g;
+  let emoteRegex = emojiRegex();
+  let chars = {
+            '0': ':zero:',
+            '1': ':one:',
+            '2': ':two:',
+            '3': ':three:',
+            '4': ':four:',
+            '5': ':five:',
+            '6': ':six:',
+            '7': ':seven:',
+            '8': ':eight:',
+            '9': ':nine:',
+            '#': ':hash:',
+            '*': ':asterisk:',
+            '?': ':grey_question:',
+            '!': ':grey_exclamation:',
+            ' ': '   '
+        }
+        const text1 = text.toLowerCase().split('').map(letter => {
+            if (/[a-z]/g.test(letter)) {
+                return `:regional_indicator_${letter}:`
+            } else if (chars[letter]) {
+                return `${chars[letter]}`
+            }
+            return letter;
+        }).join('');
+
+        return text1;
+    }
+
+
+
+
 module.exports = {
-    shipImage
+    shipImage, emojify
 }
